955b469c30d8b2735f3ead5a0dab915f805588cf
guo.feng <guo.feng@neusoft.com>
2019-05-09 13:44:26 +0800

    'Thu-May-09-2019-13:44:26-GMT+0800-(GMT+08:00)'

---
+++ src/workers/init/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const fs = require("fs");
+const path = require('path');
+const {COLOR, colorFont, run} = require('naraku/tools');
+
+module.exports = function(config, params, flags, preInfo) {
+  
+};
